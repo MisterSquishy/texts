cf43971274b48809c512740e532abd584a1f0c30
MisterSquishy <pdavids@comcast.net>
2018-02-18 09:24:59 -0500

    text message styling

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,6 +1,6 @@
 class TextsController < ApplicationController
     def index
-        @texts = Text.all.limit('20') #todo no
+        @texts = Text.all.limit('20').order(datetime: :asc) #todo no
     end
 
     def show
